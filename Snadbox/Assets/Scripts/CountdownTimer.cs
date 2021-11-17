1eba314f9141d225af090791815ba4859c022713
CodyLybbert <cody.lybbert@lethbridgecollege.ca>
2021-11-17 15:34:16 -0700

    Simple timer

---
+++ Snadbox/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    float startTime = 60f;
+    float currentTime = 0f;
+    [SerializeField]
+    Text countdownText;
+
+    private void Start()
+    {
+        currentTime = startTime;
+    }
+
+    private void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+    }
+
+
+
+}
---
+++ Snadbox/Assets/Scripts/ScoreBehaviour.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBehaviour : MonoBehaviour
+{
+    float scoreCount;
+    [SerializeField]
+    Text textScore;
+
+}
